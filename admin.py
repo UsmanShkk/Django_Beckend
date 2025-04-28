3b8360ae29d0ed22e25821e02732e135e38f6fe8
UsmanShkk <shkmusman455@gmail.com>
2025-04-28 22:08:22 +0500

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from watchlist_app.models import Watch_list,StreamPlatform,Review
+
+admin.site.register(Watch_list)
+admin.site.register(StreamPlatform)
+admin.site.register(Review)
